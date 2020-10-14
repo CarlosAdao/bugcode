8664abbfd258322c1ad77371d7fca549ef53c8ba
carlos_adao <jose.carlos.adao@hotmail.com>
2020-10-14 06:30:49 -0300

    Componentes Material Design

---
+++ app/src/main/java/com/example/snackbar/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.snackbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
