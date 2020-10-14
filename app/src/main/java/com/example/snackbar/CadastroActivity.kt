0bf650ffe4103f57076abcb2e226b0d174ba624b
carlos_adao <jose.carlos.adao@hotmail.com>
2020-10-14 06:51:21 -0300

    simple intent

---
+++ app/src/main/java/com/example/snackbar/CadastroActivity.kt
@@ -0,0 +1,11 @@
+package com.example.snackbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CadastroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+    }
+}
--- app/src/main/java/com/example/snackbar/LoginActivity.kt
+++ app/src/main/java/com/example/snackbar/LoginActivity.kt
@@ -1,11 +1,22 @@
 package com.example.snackbar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.login_body.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        btnLogin.setOnClickListener{
+            callCadastro()
+        }
+    }
+
+    //Chama a activity de cadastro
+    fun callCadastro(){
+        var intent = Intent(this, CadastroActivity::class.java)
+        startActivity(intent)
     }
 }
